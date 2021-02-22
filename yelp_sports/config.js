e53d35e1ed9040cb4b3c6589b49e3caf84c6f635
Anirudh95Kulkarni <ani995rudh@gmail.com>
2021-02-22 12:28:50 +0100

    Delete config.js

--- yelp_sports/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-	db: {
-		username: "admin",
-		connection: "mongodb+srv://admin:32Sr7jZMuV3TZfQ3@cs-sports.dzxv6.mongodb.net/yelp_sports?retryWrites=true&w=majority"
-	}
-}
-module.exports = config;
